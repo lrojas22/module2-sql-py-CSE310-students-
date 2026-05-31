3ca0967fb3c763a695a722672c3e7e83d632afcb
Luis Rojas <lrojashernandez@byupathway.edu>
2026-05-30 20:23:18 -0500

    function add students

--- main.py
+++ main.py
@@ -15,11 +15,27 @@ except Exception as e:
     print("Error",e)
 
 cursor = connection.cursor()
-cursor.execute("SELECT * FROM STUDENTS")
 
-students = cursor.fetchall()
-for student in students:
-    print(student)
+def view_students():
+    cursor.execute("SELECT * FROM STUDENTS")
 
+    students = cursor.fetchall()
+    for student in students:
+        print(student)
+
+
+def add_students():
+    
+    first_name = input("Enter first name: ")
+    last_name = input("Enter last name: ")
+    email = input("Enter email: ")
+    query = """INSERT INTO students (first_name, last_name, email)
+            VALUES (%s, %s, %s)"""
+    cursor.execute(query, (first_name,last_name,email))
+    connection.commit()
+
+
+view_students()
 cursor.close()
 connection.close()
+
